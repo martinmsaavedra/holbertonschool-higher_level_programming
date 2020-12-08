98b5aefeae390be040dd25f19b2820be0c2ba374
martinmsaavedra <martin@bildung.uy>
2020-12-08 16:38:13 +0000

    ex 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+def uppercase(str):
+    for s in str:
+        if ord(s) > 96 and ord(s) < 123:
+            s = chr(ord(s) - 32)
+            print("{}".format(s), end="")
+        else:
+            print("{}".format(s), end="")
+    print()
