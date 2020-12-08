9dae13f1bae4e6c5752b294a9fe6f3ef28036b0a
martinmsaavedra <martin@bildung.uy>
2020-12-08 17:31:00 +0000

    task 9

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(10):
         if i < j:
             print("{:d}{:d}".format(i, j), end="")
             if i < 8:
-               print(",", end=" ")
+                print(",", end=" ")
 print()
