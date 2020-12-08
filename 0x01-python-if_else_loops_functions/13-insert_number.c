ff40ff55ae6797dc73326c5843a2efa3388294b9
martinmsaavedra <martin@bildung.uy>
2020-12-08 23:08:09 +0000

    ex 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,58 +4,42 @@
  *
  *
  */
-listint_t *add_node_position(listint_t *node, int number)
-{
-	listint_t *new;
-	new = malloc(sizeof(listint_t));
-        if (!new)
-                return (NULL);
-
-	new->next = node->next;
-	new->n = number;
-	node->next = new;
-	return (new);
-}
 
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *aux;
+	
+	new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
 
-	if (!number)
-		return (NULL);
 
 	if (!head)
 		return (NULL);
 
 	if (*head == NULL)
-		{
-			new = malloc(sizeof(listint_t));
-			if (!new)
-				return (NULL);
-
-			new->n = number;
-			new->next = *head;
-			*head = new;
-
-			return (new);
-		}
-	aux = *head;
-
-	while (aux->n < number)
 	{
-		aux = aux->next;
-	}
-	if (aux->next == NULL)
-		return (add_nodeint_end(head, number));
-	else
-	{
-		new = malloc(sizeof(listint_t));
-        if (!new)
-                return (NULL);
-
-		new->next = aux->next;
+			
 		new->n = number;
-		aux->next = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
+	aux = *head;
+	while (aux->next)
+	{
+		if (aux->n <= number && aux->next->n >= number)
+		{
+			new->next = aux->next;
+			new->n = number;
+			aux->next = new;
+			
+			return (new);
+		}
+		aux = aux->next;
+	}
+	aux->next = new;
+	/*if (aux->next == NULL)
+		return (add_nodeint_end(head, number));*/
+	return (new);
 }
