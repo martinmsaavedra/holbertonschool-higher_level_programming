6c1089069e2ad2c996e199eb45ff7862165cfbf2
martinmsaavedra <martin@bildung.uy>
2020-12-10 16:55:09 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     token = dir(hidden_4)
     for s in range(len(token)):
-         if token[s][:2] != "__":
-             print(token[s])
+        if token[s][:2] != "__":
+            print(token[s])
