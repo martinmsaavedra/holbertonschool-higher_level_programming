e4f7e9ef7ff521ee92b8107d4d991ba24de49590
martinmsaavedra <martin@bildung.uy>
2020-12-15 20:46:55 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,16 +2,19 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
     lists = []
-    for i in range(0, 2):
-        if len(tuple_a) < 2:
-            a = 0
-        else:
-            a = tuple_a[i]
-        if len(tuple_b) < 2:
-            b = 0
-        else:
-            b = tuple_b[i]
-        x = a + b
-        lists.append(x)
-    new_tuple = tuple(lists)
-    return new_tuple
+
+    if len(tuple_a) > 1 and len(tuple_b) > 1:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        return new_tuple
+    else:
+        for i in range(0, 2):
+            if tuple_a[i]:
+                lists.append(tuple_a[i])
+            else:
+                lists.append(0)
+            if len(tuple_b) >= 1 and i < len(tuple_b):
+                lists.append(tuple_b[i])
+            else:
+                lists.append(0)
+        new_tuple = (lists[0] + lists[1], lists[2] +lists[3])
+        return new_tuple
