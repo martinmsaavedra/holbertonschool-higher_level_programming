06504af928dfdf5586b6dbcf90ca6660eb0ca700
Martin Saavedra <45432462+martinmsaavedra@users.noreply.github.com>
2020-12-17 19:01:49 -0300

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def complex_delete(a_dictionary, value):
-    for key, value2 in list(a_dictionary.items()):
+    for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
