9b2ee0d1de1c12bc09586e72b27b7c5d6e1451a9
Martin Saavedra <45432462+martinmsaavedra@users.noreply.github.com>
2020-12-17 17:36:37 -0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     if type(roman_string) is not str or roman_string is None:
         return 0
