99c817a9993d093b98129fb8c9c778158c1def22
martinmsaavedra <martin@bildung.uy>
2020-12-17 19:18:09 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,11 @@
 def best_score(a_dictionary):
     maximo = 0
     llave = ""
-    if a_dictionary is not None:
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    else:
         for clave, valor in a_dictionary.items():
             if valor > maximo:
                 llave = clave
                 maximo = valor
         return llave
-    else:
-        return None
