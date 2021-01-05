4c9a478be8831700454931bcb182f0016f7455a2
martinmsaavedra <martin@bildung.uy>
2021-01-05 18:54:28 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,8 @@ class Square:
 
     def __init__(self, size=0):
         """Square constructor"""
-        try:
-            int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
