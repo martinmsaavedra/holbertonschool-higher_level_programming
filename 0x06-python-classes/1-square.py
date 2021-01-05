73da5256985af23f097938365db197089aabccc7
martinmsaavedra <martin@bildung.uy>
2021-01-05 18:49:17 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Square Class"""
+
 class Square:
-    """Square class"""
+    """ Square class """
     def __init__(self, size=0):
         """Square constructor"""
         self.__size = size
