e38d7cff8f556efc316aa12c08a159a59b4cb017
martinmsaavedra <martin@bildung.uy>
2021-01-08 22:33:58 +0000

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,5 +6,33 @@ if __name__ == "__main__":
     doctest.testfile("tests/2-matrix_divided.txt")
 
 def matrix_divided(matrix, div):
-    """TDD module"""
+    """
+    Divide matrix by div and return new matrix with new values
+    """
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(matrix) != list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     
+    largo = len(matrix[0])
+    if largo == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    for elem in matrix:
+        if largo != len(elem):
+            raise TypeError("Each row of the matrix must have the same size")
+        if type(elem) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for val in elem:
+                if type(val) != float and type(val) != int:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        
+    new_matrix = []
+    for elem in range(len(matrix)):
+        new_matrix.append([])
+        for item in matrix[elem]:
+            new_matrix[elem].append(round((item / div), 2))   
+    
+    return new_matrix
