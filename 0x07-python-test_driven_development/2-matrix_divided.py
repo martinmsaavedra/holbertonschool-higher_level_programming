4d4442e32914bb7e531538384fd17b21f372ecdc
martinmsaavedra <martin@bildung.uy>
2021-01-08 22:40:44 +0000

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """TDD module"""
 
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/2-matrix_divided.txt")
+
 
 def matrix_divided(matrix, div):
     """
@@ -14,25 +16,28 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+        raise TypeError("matrix must be a matrix (list of lists) \
+        of integers/floats")
+
     largo = len(matrix[0])
     if largo == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+        of integers/floats")
 
     for elem in matrix:
         if largo != len(elem):
             raise TypeError("Each row of the matrix must have the same size")
         if type(elem) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of \
+            integers/floats")
         for val in elem:
                 if type(val) != float and type(val) != int:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+                    raise TypeError("matrix must be a matrix (list of lists)\
+                    of integers/floats")
+
     new_matrix = []
     for elem in range(len(matrix)):
         new_matrix.append([])
         for item in matrix[elem]:
-            new_matrix[elem].append(round((item / div), 2))   
-    
+            new_matrix[elem].append(round((item / div), 2))
     return new_matrix
