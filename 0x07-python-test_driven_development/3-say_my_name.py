371563b23c5aaf6fbe05e59d6ac76994aaf69450
martinmsaavedra <martin@bildung.uy>
2021-01-08 23:34:07 +0000

    task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""TDD module"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
+
+
+def say_my_name(first_name, last_name=""):
+    """
+         prints My name is <first name> <last name>
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
