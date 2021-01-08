7d0a6d1fb8e1db9bd0782f8ff936e28d928b483c
martinmsaavedra <martin@bildung.uy>
2021-01-08 21:38:48 +0000

    task 0

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""TDD module"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
+
+def matrix_divided(matrix, div):
+    """TDD module"""
+    
