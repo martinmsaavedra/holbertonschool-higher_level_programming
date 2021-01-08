790ba9944235854cff032460c287776d643a82f2
martinmsaavedra <martin@bildung.uy>
2021-01-08 17:57:05 +0000

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""TDD module"""
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
+
+
 def add_integer(a, b=98):
+    """
+    Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b) != float:
