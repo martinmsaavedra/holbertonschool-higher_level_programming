f6d0dfff2e079ce967b59def3060f069d9463ccb
martinmsaavedra <martin@bildung.uy>
2021-01-12 01:32:25 +0000

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,34 +11,29 @@ def matrix_divided(matrix, div):
     """
     Divide matrix by div and return new matrix with new values
     """
-    if matrix is None:
-        raise TypeError
-    if div is None:
-        raise TypeError
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(matrix) != list:
         raise TypeError("matrix must be a matrix (array of arrays \
-                         of integers/floats")
+of integers/floats")
 
     largo = len(matrix[0])
     if largo == 0:
         raise TypeError("matrix must be a matrix (array of arrays \
-                         of integers/floats")
+of integers/floats")
 
     for elem in matrix:
         if largo != len(elem):
             raise TypeError("Each row of the matrix must have the same size")
         if type(elem) != list:
             raise TypeError("matrix must be a matrix (array of arrays \
-                             of integers/floats")
+of integers/floats")
         for val in elem:
                 if type(val) != float and type(val) != int:
                     raise TypeError("matrix must be a matrix (array of arrays \
-                                     of integers/floats)")
-
+of integers/floats)")
     new_matrix = []
     for elem in range(len(matrix)):
         new_matrix.append([])
