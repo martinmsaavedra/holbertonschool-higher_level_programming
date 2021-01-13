a8806cf63219abc4434e4b381fb3122cc74cea51
martinmsaavedra <martin@bildung.uy>
2021-01-13 00:42:40 +0000

    Everything is a porject

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
+    """
+
+    __slots__ = ["first_name"]
