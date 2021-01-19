e2459117b05474790d446653a7de65eef9302a5f
martinmsaavedra <martin@bildung.uy>
2021-01-19 21:15:10 +0000

    task 4 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+'''inhertis_from module'''
 
 
 def inherits_from(obj, a_class):
-    if obj.__class__ == a_class:
+    '''inherits_from function'''
+
+    if type(obj) == a_class:
         return False
     else:
         return isinstance(obj, a_class)
