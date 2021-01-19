5961f2e9b67ccbfe0e33d1bfeca1c2900464ea73
martinmsaavedra <martin@bildung.uy>
2021-01-19 22:02:40 +0000

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,25 +4,6 @@
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    '''Rectangle class'''
-
-    def __init__(self, width, height):
-        '''Constructor function'''
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        '''Returns area of rectangle'''
-        return self.__width * self.__height
-
-    def __str__(self):
-        '''prints object description'''
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
-
 class Square(Rectangle):
     '''Square class'''
 
