65265469c992181dc4e92363ec25602e060ea0bc
martinmsaavedra <martin@bildung.uy>
2021-01-20 21:41:59 +0000

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,11 +5,11 @@
 def append_after(filename="", search_string="", new_string=""):
     '''function that inserts a line of text to a file, after
     each line containing a specific string'''
-    
+
     input_file = open(filename, mode='r').readlines()
     with open(filename, mode="w") as f:
         for line in input_file:
             f.write(line)
-            if search_string in  line:
+            if search_string in line:
                 f.write(new_string)
         f.close()
