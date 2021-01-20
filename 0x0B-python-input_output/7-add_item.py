72e69ae5e31e8df68ec0e17e1c42c0877e2af718
martinmsaavedra <martin@bildung.uy>
2021-01-20 20:30:51 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-'''script that adds all arguments to a Python list,
-and then save them to a file'''
+"""Adds all arguments to a Python list, and then save them to a file"""
 import json
 import sys
 import os.path
+load_from_json_file = __import__('6-load_from_json_file')
+.load_from_json_file
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file')
+.save_to_json_filemy_list = []
 
-args = []
 i = 0
-
+for x in sys.argv:
+    if i is not 0:
+        my_list.append(x)
+    i += 1
 filename = "add_item.json"
-if os.path.exists(filename):
+
+try:
     data = load_from_json_file(filename)
-    for ilem in data:
-        args.append(ilem)
-
-    for elem in sys.argv:
-        if i is not 0:
-            args.append(elem)
-        i = + 1
-    save_to_json_file(args, filename)
-
+    my_list = data + my_list
+except:
+    save_to_json_file(my_list, filename)
 else:
-    for elem in sys.argv:
-        if i is not 0:
-            args.append(elem)
-        i = + 1
-
-    save_to_json_file(args, filename)
+    save_to_json_file(my_list, filename)
