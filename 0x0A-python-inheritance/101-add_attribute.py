0e6aa375f033bb189721911738800f5913b62c37
martinmsaavedra <martin@bildung.uy>
2021-01-20 02:59:13 +0000

    pep8 fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Adds a new attribute to an object if it’s possible"""
 
+
 def add_attribute(object, name, value):
     """Adds a new attribute"""
     if not hasattr(object, 'name'):
-       setattr(object, name, value)
+        setattr(object, name, value)
     else:
         raise TypeError("can't add new attribute")
-    
