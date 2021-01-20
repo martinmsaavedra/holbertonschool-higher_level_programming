0c349d52d173a1e725d5b35f6c2e967426c0041d
martinmsaavedra <martin@bildung.uy>
2021-01-20 21:40:05 +0000

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''after party module'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''function that inserts a line of text to a file, after
+    each line containing a specific string'''
+    
+    input_file = open(filename, mode='r').readlines()
+    with open(filename, mode="w") as f:
+        for line in input_file:
+            f.write(line)
+            if search_string in  line:
+                f.write(new_string)
+        f.close()
