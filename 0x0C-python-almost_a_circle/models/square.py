74e3d855f702a20441a7eb2963a8f4d16845510a
martinmsaavedra <martin@bildung.uy>
2021-01-27 12:57:05 +0000

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,26 +9,26 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Square constructor'''
         super().__init__(size, size, x, y, id)
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """y Getter"""
-        return self.__size
+        """size Getter"""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """Width Setter
+        """size Setter
         """
         self.int_validator("width", value)
-        self.__size = value
+        self.width = value
 
     def __str__(self):
         '''Str method'''
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self._Rectangle__x,
                                                  self._Rectangle__y,
-                                                 self.__size)
+                                                 self.size)
 
     def update(self, *args, **kwargs):
         """args and kwargs"""
