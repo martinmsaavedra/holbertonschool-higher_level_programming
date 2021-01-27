2047b9a2bfe66368dd5f5bb1ac5820da45201ae5
martinmsaavedra <martin@bildung.uy>
2021-01-27 13:12:09 +0000

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     def to_json_string(list_dictionaries):
         '''json dumps'''
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return []
         return json.dumps(list_dictionaries)
 
     '''@classmethod
