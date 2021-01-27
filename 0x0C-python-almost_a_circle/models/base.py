9303293d91a570600a30d67f4bf469130a759bc8
martinmsaavedra <martin@bildung.uy>
2021-01-27 13:31:15 +0000

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,11 +18,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        '''json dumps'''
-        if list_dictionaries is None or not list_dictionaries:
+        """ function to_json_string """
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     '''@classmethod
     def save_to_file(cls, list_objs):
