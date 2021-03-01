66c5a2c62c64b51475dcb630141c770bf693d5b2
martinmsaavedra <martin@bildung.uy>
2021-03-01 16:53:36 +0000

    fix 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table(id, name, score)
+INSERT INTO second_table
 VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
