fbd56a0569ebf790b41d0a6af3b63c247d6840bf
martinmsaavedra <martin@bildung.uy>
2021-03-02 17:40:39 +0000

    sql more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creates the MySQL server user user_0d_1. 
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR  'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
