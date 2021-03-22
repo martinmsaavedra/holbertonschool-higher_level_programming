62739531dcbb5f8450bd97ba252ac76d81339bbd
martinmsaavedra <martin@bildung.uy>
2021-03-22 15:41:17 +0000

    task 0 and 1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.lenght === 2) {
   console.log('No argument');
 }
-if (process.argv.lenght === 3) {
+else if (process.argv.lenght === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
