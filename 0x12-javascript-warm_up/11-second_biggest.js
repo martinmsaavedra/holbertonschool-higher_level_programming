32621c70f1973bf55e7c8f5349d68c2fdd9755f0
martinmsaavedra <martin@bildung.uy>
2021-03-22 16:29:02 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,5 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   process.argv.sort((a, b) => a - b);
-  console.log(process.argv.sort((a, b) => a - b));
   console.log(process.argv[process.argv.length - 2]);
 }
