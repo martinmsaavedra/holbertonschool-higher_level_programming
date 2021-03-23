47a801c7969346af49585155e343dcd1604c5de6
martinmsaavedra <martin@bildung.uy>
2021-03-23 15:48:00 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,12 +8,14 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
+      this.print();
     }
-    for (let x = 0; x < this.height; x++) {
-      let x = '';
-      for (let y = 0; y < this.width; y++) x += c;
-      console.log(x);
+    else {
+      for (let x = 0; x < this.height; x++) {
+        let x = '';
+        for (let y = 0; y < this.width; y++) x += c;
+        console.log(x);
+      }
     }
   }
 }
