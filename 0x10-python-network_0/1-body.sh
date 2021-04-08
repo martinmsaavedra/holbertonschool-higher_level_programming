933926199875ca3210a6fa8603d2598487586909
martinmsaavedra <martin@bildung.uy>
2021-04-08 17:10:55 +0000

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ $(curl -s -o /dev/null -I -w "%{http_code}" $1) -eq "200" ]; then curl -sX GET $1; fi
+curl -sL $1
