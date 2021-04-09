5897b3d25f4aab6c80a873301b513f98c49946e6
martinmsaavedra <martin@bildung.uy>
2021-04-09 17:38:58 +0000

    task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,8 @@ from urllib import parse
 
 
 if __name__ == "__main__":
-    args = {'email':sys.argv[2]}
+    args = {'email': sys.argv[2]}
     data = parse.urlencode(args).encode('utf-8')
     with urllib.request.urlopen(sys.argv[1], data) as response:
         html = response.read().decode('utf-8')
     print(html)
-
