9b8b75339e012c315285808791d601fb000550a3
martinmsaavedra <martin@bildung.uy>
2021-04-09 21:28:37 +0000

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/" + user + "/" + repo + "/commits"
     response = requests.get(url)
     list_commits = response.json()
-    for elem in list_commits:
-        sha = elem.get('parents')[0].get('sha')
-        name = elem.get('commit').get('author').get('name')
-        print(sha, name)
+    for index, elem in enumerate(list_commits):
+        if index <= 9:
+            sha = elem.get('parents')[0].get('sha')
+            name = elem.get('commit').get('author').get('name')
+            print("{}: {}".format(sha, name))
+        else:
+            break
