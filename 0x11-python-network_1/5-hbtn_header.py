73ba891bc699362c34df7c4d70acbd080189f045
martinmsaavedra <martin@bildung.uy>
2021-04-09 19:14:59 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''network module'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers['x-request-id'])
