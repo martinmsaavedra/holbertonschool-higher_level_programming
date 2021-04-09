efd514e3297fa5c427ae9efeb94c2d278564cdf2
martinmsaavedra <martin@bildung.uy>
2021-04-09 18:29:18 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 
 if __name__ == '__main__':
     response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:\t- type: {}\t- content: {}'.
+    print('Body response:\n\t- type: {}\n\t- content: {}'.
           format(type(response.text), response.text))
