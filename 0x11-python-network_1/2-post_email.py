3ceaae1f4e3f69891fe69bb033964bf211ac327d
martinmsaavedra <martin@bildung.uy>
2021-04-09 17:37:13 +0000

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''network module'''
+import sys
+import urllib.request
+from urllib import parse
+
+
+if __name__ == "__main__":
+    args = {'email':sys.argv[2]}
+    data = parse.urlencode(args).encode('utf-8')
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        html = response.read().decode('utf-8')
+    print(html)
+
