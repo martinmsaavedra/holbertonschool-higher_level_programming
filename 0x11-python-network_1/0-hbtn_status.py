db9f04e02d6c4e260a87bb7c87b6a593afbea1ca
martinmsaavedra <martin@bildung.uy>
2021-04-09 16:12:25 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+asdsda#!/usr/bin/python3
+'''network module'''
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status')as response:
+   html = response.read()
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t -utf8 content: {}".format(type(html), html, html.decode()))
