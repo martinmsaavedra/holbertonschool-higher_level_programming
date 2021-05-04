78da71064ddbedc02c75b0d9c9fb8b739dc3af43
Martin Saavedra <martin@bildung.uy>
2021-05-04 16:47:05 +0000

    task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    print(characters, 0);
-  }
-});
 
-function print (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        print(characters, index + 1);
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const path = JSON.parse(body).characters;
+  path.forEach(element => {
+    request(element, function (error, response, body) {
+      if (error) {
+        console.error(error);
       }
-    }
+      const name = JSON.parse(body).name;
+      console.log(name);
+    });
   });
-}
+});
